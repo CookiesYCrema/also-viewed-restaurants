387a8b76d15e5c75e4d296c8857c3f6a34c8b770
Mario <mario.santamaria.portilla@gmail.com>
2018-08-17 20:45:14 -0700

    a lot of controller changes necesary for CRUD

--- server/controller.js
+++ server/controller.js
@@ -1,32 +1,103 @@
 // const db = require('../db/postgres/')
 const { Place } = require('../db/postgres/model');
-const { PlaceC } = require('../db/cassandra/model');
 
 const controller = {
-  get: (req, res) => {
-    console.log(req.query)
-    res.status(200).send();
+  id: {
+    get: (req, res) => {
+      console.log(req.params)
+      Place.findOne({
+        where: { 
+          id: req.params.id
+        }
+      })
+      .then(place => {
+        res.status(200).send(place);
+      })
+      .catch(error => {
+        console.error('Error getting the place with id =>', error);
+        res.status(400).send();
+      });
+    },
+
+    delete: (req, res) => {
+      console.log(req.params)
+      Place.destroy({
+        where: {
+          id: req.params.id
+        }
+      })
+      .then(place => {
+        res.status(202).send(place);
+      })
+      .catch(error => {
+        console.error('Error deleting place =>', error);
+        res.status(400).send();
+      });
+    },
+
+    put: (req, res) => {
+      Place.update({
+        rating: req.body.rating,
+      }, {
+        where: {
+          id: req.params.id
+        }
+      })
+      .then(place => {
+        console.log(place.length)
+        if(place.length !== 0) {
+          res.status(202).send({place});
+        } else {
+          res.status(204).send(place);
+        }
+      })
+      .catch(error => {
+        console.error('Error updating place =>', error);
+        res.status(400).send();
+      });
+    }
   },
-  post: (req, res) => {
-    Place.create({
-      name: req.body.name,
-      reviews: req.body.reviews,
-      rating: req.body.rating,
-      price: req.body.price,
-      mainCategory: req.body.mainCategory,
-      subCategory: req.body.subCategory,
-      city: req.body.city,
-      image: req.body.image
-    })
-    .then((place) => {
-      console.log('Creating place:', place)
-      res.status(201).send(place);
-    })
-    .catch(error => {
-      console.error('Error creating place', error);
-      res.status(400).send();
-    });
-  }
+
+  mainCategoryCity: {
+    get: (req, res) => {
+      console.log(req.params)
+      Place.findAll({
+        where: { 
+          mainCategory: req.params.mainCategory,
+          city: req.params.city
+        },
+        limit: 10
+      })
+      .then(places => {
+        res.status(200).send(places);
+      })
+      .catch(error => {
+        console.error('Error getting place with mainCategory and city =>', error);
+        res.status(400).send();
+      });
+    },
+
+    post: (req, res) => {
+      Place.create({
+        name: req.body.name,
+        reviews: req.body.reviews,
+        rating: req.body.rating,
+        price: req.body.price,
+        mainCategory: req.body.mainCategory,
+        subCategories: req.body.subCategories,
+        city: req.body.city,
+        image: req.body.image
+      })
+      .then((place) => {
+        console.log('Creating place:', place)
+        res.status(201).send({id: place.id});
+      })
+      .catch(error => {
+        console.error('Error creating place =>', error);
+        res.status(400).send();
+      });
+    }
+  },
 }
 
 module.exports = controller;
--- server/router.js
+++ server/router.js
@@ -1,10 +1,13 @@
 const router = require('express').Router();
 const controller = require('./controller.js');
 
-router.route('/also-viewed')
-.get(controller.get);
+router.route('/also-viewed/:id')
+.get(controller.id.get)
+.delete(controller.id.delete)
+.put(controller.id.put);
 
-router.route('/also-viewed')
-.post(controller.post);
+router.route('/also-viewed/:mainCategory?/:city?')
+.get(controller.mainCategoryCity.get)
+.post(controller.mainCategoryCity.post);
 
 module.exports = router;
