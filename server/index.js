d2f422a2b86010fe841ec0128acd854ffd8b1963
Mario <mario.santamaria.portilla@gmail.com>
2018-08-24 16:49:33 -0700

    tested nginx

--- server/index.js
+++ server/index.js
@@ -4,15 +4,17 @@ const path = require('path');
 const parser = require('body-parser');
 const router = require('./router.js');
 const cors = require('cors');
-const helmet = require('helmet')
+const helmet = require('helmet');
+const morgan = require('morgan');
 
 const app = express();
 const port = 3000;
 
-app.use(parser.json());
-app.use(parser.urlencoded({extended: true}));
-app.use(helmet());
-app.use(cors());
+app.use(morgan('dev'));
+// app.use(parser.json());
+// app.use(parser.urlencoded({extended: true}));
+// app.use(helmet());
+// app.use(cors());
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use('/api', router);
